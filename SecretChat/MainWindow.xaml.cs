bcf2fa03a2ec42a63c8a1001f9ed29de16a48ea3
spvce <dujldo.7@gmail.com>
2023-04-20 19:05:27 +0200

    Build 0.1

--- SecretChat/MainWindow.xaml.cs
+++ SecretChat/MainWindow.xaml.cs
@@ -1,14 +1,100 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace SecretChat
 {
     public partial class MainWindow : Window
     {
-        bool running = false;
+
+        private TcpClient? _client;
+        private SslStream? _sslStream;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string serverIP = "127.0.0.1";
+            int serverPort = 5000;
+
+            // Connect to the server
+            _client = new TcpClient();
+            await _client.ConnectAsync(serverIP, serverPort);
+
+            _sslStream = new SslStream(_client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+
+            // Authenticate the client
+            X509Certificate2 clientCertificate = new X509Certificate2("E:\\Cybersec\\Secret-Chat-On-CSharp\\SecretChat\\server.pfx", "4-puklvife#9");
+            var clientCertificates = new X509CertificateCollection(new X509Certificate[] { clientCertificate });
+            await _sslStream.AuthenticateAsClientAsync("127.0.0.1", clientCertificates, SslProtocols.Tls12, false);
+
+            // Send the unique client ID to the server for registration
+            byte[] clientIdBytes = Encoding.UTF8.GetBytes(ClientIdTextBox.Text);
+            await _sslStream.WriteAsync(clientIdBytes, 0, clientIdBytes.Length);
+
+            // Set the UI state to connected
+            ConnectButton.IsEnabled = false;
+            SendMessageButton.IsEnabled = true;
+            DisconnectButton.IsEnabled = true;
+
+            // Start listening for messages
+            _ = ListenForMessagesAsync();
+        }
+
+        private async Task ListenForMessagesAsync()
+        {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            try
+            {
+                while ((bytesRead = await _sslStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Dispatcher.Invoke(() => ChatTextBox.AppendText($"{message}\n"));
+                }
+            }
+            catch (IOException)
+            {
+                // The connection was closed
+            }
+        }
+
+        private async void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(RecipientIdTextBox.Text) && !string.IsNullOrEmpty(MessageTextBox.Text))
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes($"{RecipientIdTextBox.Text}|{MessageTextBox.Text}");
+                await _sslStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+                ChatTextBox.AppendText($"You: {MessageTextBox.Text}\n");
+                MessageTextBox.Clear();
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _client?.Close();
+            _sslStream?.Dispose();
+
+            ConnectButton.IsEnabled = true;
+            SendMessageButton.IsEnabled = false;
+            DisconnectButton.IsEnabled = false;
+        }
+
+        private static bool ValidateServerCertificate(object sender, X509Certificate? certificate, X509Chain? chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // In a real application, you should validate the server certificate properly
+            return true;
         }
     }
+
 }
---
+++ ServerForChat/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSecureChatServer
+{
+    class Program
+    {
+        private static ConcurrentDictionary<string, SslStream> _connectedClients = new ConcurrentDictionary<string, SslStream>();
+
+        static async Task Main(string[] args)
+        {
+            // Set server IP and port
+            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+            int serverPort = 5000;
+
+            // Load the server certificate
+            X509Certificate2 serverCertificate = new X509Certificate2("server.pfx", "4-puklvife#9");
+
+            // Create a TcpListener
+            TcpListener server = new TcpListener(serverIP, serverPort);
+            server.Start();
+
+            Console.WriteLine("Server started, waiting for clients...");
+
+            while (true)
+            {
+                // Accept a new client
+                TcpClient client = await server.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client, serverCertificate); // Handle client in a separate task
+            }
+        }
+
+        static async Task HandleClientAsync(TcpClient client, X509Certificate2 serverCertificate)
+        {
+            string clientId = null;
+            using (var sslStream = new SslStream(client.GetStream(), false))
+            {
+                // Authenticate the server
+                await sslStream.AuthenticateAsServerAsync(serverCertificate, false, SslProtocols.Tls12, false);
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                // Register the client and store its ID
+                bytesRead = await sslStream.ReadAsync(buffer, 0, buffer.Length);
+                clientId = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                _connectedClients.TryAdd(clientId, sslStream);
+                Console.WriteLine($"Client '{clientId}' connected.");
+
+                while ((bytesRead = await sslStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    // Process the received message
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    string[] messageParts = message.Split('|');
+
+                    if (messageParts.Length == 2)
+                    {
+                        string recipientId = messageParts[0];
+                        string messageContent = messageParts[1];
+
+                        if (_connectedClients.TryGetValue(recipientId, out SslStream recipientStream))
+                        {
+                            byte[] messageBytes = Encoding.UTF8.GetBytes($"{clientId}: {messageContent}");
+                            await recipientStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Message routing failed. Recipient '{recipientId}' not found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid message format received.");
+                    }
+                }
+            }
+
+            if (clientId != null)
+            {
+                _connectedClients.TryRemove(clientId, out _);
+                Console.WriteLine($"Client '{clientId}' disconnected.");
+            }
+        }
+    }
+}
