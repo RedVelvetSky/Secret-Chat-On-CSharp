805549f7092bd01a901971689b649fc0ffc8276c
spvce <dujldo.7@gmail.com>
2023-05-03 22:31:21 +0200

    fixin'

--- SecretChat/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ SecretChat/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -108,7 +108,7 @@ namespace SecretChat {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SecretChat;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SecretChat;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
